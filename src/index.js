9d09c890dc714dcf930c23bf52d8b6420e2b4d0c
Ritika Rawat <Ritikarawat220@gmail.com>
2023-03-14 13:12:08 +0530

    Add placeholders in index.js:

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./index.css";
+import itemList from "./itemList";
+
+const placeholders = [
+    {
+        name: "Name",
+        score: 20,
+    },
+    {
+        name: "Name",
+        score: 40,
+    },
+    {
+        name: "Name",
+        score: 50,
+    },
+    {
+        name: "Name",
+        score: 10,
+    },
+    {
+        name: "Name",
+        score: 100,
+    },
+];
+
+itemList(placeholders);
