dbe11e0f64e5466e3dc2564a3db9a6cc05bd0e11
Ritika Rawat <Ritikarawat220@gmail.com>
2023-03-15 19:43:29 +0530

    Add eventlistener for refreshh button

--- src/index.js
+++ src/index.js
@@ -3,3 +3,27 @@ import itemList from "./itemList";
 import { getScores, postScores } from "./request";
 
 const TEST_GAME_ID = "ubc1A0KC5FEKLnzaXFow";
+
+const refreshButton = document.getElementById("refresh-btn");
+const form = document.getElementById("form");
+
+refreshButton.addEventListener("click", () => {
+  getScores(TEST_GAME_ID).then((scores) => {
+    itemList(scores.result);
+  });
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const body = {
+    user: document.getElementById("name-input").value,
+    score: parseInt(document.getElementById("score-input").value, 10),
+  };
+  postScores(TEST_GAME_ID, body);
+  document.getElementById("name-input").value = "";
+  document.getElementById("score-input").value = "";
+});
+
+getScores(TEST_GAME_ID).then((scores) => {
+  itemList(scores.result);
+});
