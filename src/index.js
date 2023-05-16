01eeda3a1f4db7602e97edb6a536745d83b7e6e8
Ritika Rawat <Ritikarawat220@gmail.com>
2023-05-16 15:06:43 +0530

    Modified changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./index.css";
 import itemList from "./itemList";
 import { getScores, postScores } from "./request";
 
-const TEST_GAME_ID = "ubc1A0KC5FEKLnzaXFow";
+const TEST_GAME_ID = "ubc1A0KC5FEKLnsszaXFow";
 
 const refreshButton = document.getElementById("refresh-btn");
 const form = document.getElementById("form");
@@ -15,14 +15,30 @@ refreshButton.addEventListener("click", () => {
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const body = {
-    user: document.getElementById("name-input").value,
-    score: parseInt(document.getElementById("score-input").value, 10),
-  };
+  const nameInput = document.getElementById("name-input");
+  const scoreInput = document.getElementById("score-input");
+  const name = nameInput.value;
+  const score = parseInt(scoreInput.value, 10);
+
+  // Check that the name and score have no more than 20 letters and 10 digits
+  if (name.length > 20) {
+    alert("Name must have no more than 20 letters");
+    return;
+  }
+  if (score.toString().length > 10) {
+    alert("Score must have no more than 10 digits");
+    return;
+  }
+
+  // If the name and score are valid, post the score to the server
+  const body = { user: name, score };
   postScores(TEST_GAME_ID, body);
-  document.getElementById("name-input").value = "";
-  document.getElementById("score-input").value = "";
+
+  // Clear the name and score inputs
+  nameInput.value = "";
+  scoreInput.value = "";
 });
+
 
 getScores(TEST_GAME_ID).then((scores) => {
   const { result } = scores;
